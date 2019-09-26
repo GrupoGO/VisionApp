fd366469dde34408debaf065b0c8523f0dd79280
Grupo Go Optimizations <globalapps@globalincubator.net>
2019-09-26 12:29:22 +0200

    QR scan

--- VisionApp/VALoginVC.swift
+++ VisionApp/VALoginVC.swift
@@ -20,6 +20,12 @@ class VALoginVC: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var activityLoader: UIActivityIndicatorView!
     
+    @IBOutlet weak var scannerView: QRScannerView! {
+        didSet {
+            scannerView.delegate = self
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +58,21 @@ class VALoginVC: UIViewController {
         let tapGestureRecognizer : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyBoard))
         self.view.addGestureRecognizer(tapGestureRecognizer)
         
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if !scannerView.isRunning {
+            scannerView.startScanning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if !scannerView.isRunning {
+            scannerView.stopScanning()
+        }
     }
 
     @objc func dismissKeyBoard() {
@@ -215,6 +236,166 @@ extension VALoginVC: UITextFieldDelegate {
         }
         
         return true
+    }
+    
+}
+
+extension VALoginVC: QRScannerViewDelegate {
+    
+    func qrScanningDidFail() {
+        
+    }
+    
+    func qrScanningSucceededWithCode(_ str: String?) {
+        if let str = str, str.split(separator: ".").count == 3 {
+            UserDefaults.standard.set(str.split(separator: ".")[0], forKey: "VAcurrentUserID")
+            UserDefaults.standard.set(str.split(separator: ".")[1], forKey: "VAcurrentUserCode")
+            UserDefaults.standard.set(str.split(separator: ".")[2], forKey: "VAcurrentUserProfileCode")
+            
+            self.activityLoader.startAnimating()
+            self.recoveryPasswordButton.isEnabled = false
+            self.cancelButton.isEnabled = false
+            scannerView.stopScanning()
+
+            VARequestManager.shared.getSessionUser { (success, message, user) in
+                self.activityLoader.stopAnimating()
+                self.recoveryPasswordButton.isEnabled = true
+                self.cancelButton.isEnabled = true
+                if let user = user {
+                    VASessionManager.shared.setUserInfo(user)
+                    self.gotoApp()
+                } else {
+                    self.scannerView.startScanning()
+                    self.alertErrorMessage(message: message)
+                }
+            }
+
+        } else {
+            print("scan error")
+        }
+        
+    }
+    
+    func qrScanningDidStop() {
+        
+    }
+    
+    
+}
+
+protocol QRScannerViewDelegate: class {
+    func qrScanningDidFail()
+    func qrScanningSucceededWithCode(_ str: String?)
+    func qrScanningDidStop()
+}
+
+
+import AVFoundation
+
+class QRScannerView: UIView {
+    
+    weak var delegate: QRScannerViewDelegate?
+    
+    /// capture settion which allows us to start and stop scanning.
+    var captureSession: AVCaptureSession?
+    
+    // Init methods..
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        doInitialSetup()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        doInitialSetup()
+    }
+    
+    //MARK: overriding the layerClass to return `AVCaptureVideoPreviewLayer`.
+    override class var layerClass: AnyClass  {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    override var layer: AVCaptureVideoPreviewLayer {
+        return super.layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+extension QRScannerView {
+    
+    var isRunning: Bool {
+        return captureSession?.isRunning ?? false
+    }
+    
+    func startScanning() {
+       captureSession?.startRunning()
+    }
+    
+    func stopScanning() {
+        captureSession?.stopRunning()
+        delegate?.qrScanningDidStop()
+    }
+    
+    /// Does the initial setup for captureSession
+    private func doInitialSetup() {
+        clipsToBounds = true
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch let error {
+            print(error)
+            return
+        }
+        
+        if (captureSession?.canAddInput(videoInput) ?? false) {
+            captureSession?.addInput(videoInput)
+        } else {
+            scanningDidFail()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession?.canAddOutput(metadataOutput) ?? false) {
+            captureSession?.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr, .ean8, .ean13, .pdf417]
+        } else {
+            scanningDidFail()
+            return
+        }
+        
+        self.layer.session = captureSession
+        self.layer.videoGravity = .resizeAspectFill
+        
+        captureSession?.startRunning()
+    }
+    
+    func scanningDidFail() {
+        delegate?.qrScanningDidFail()
+        captureSession = nil
+    }
+    
+    func found(code: String) {
+        delegate?.qrScanningSucceededWithCode(code)
+    }
+    
+}
+
+extension QRScannerView: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+        stopScanning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
     }
     
 }
